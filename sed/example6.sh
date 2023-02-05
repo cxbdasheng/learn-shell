a1493e7e9323c8a6ab9806d43611853d60bc5f45
Dasheng Chen <40051171+cxbdasheng@users.noreply.github.com>
2023-02-05 20:44:12 +0800

    利用 sed 删除特定内容

---
+++ sed/example6.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# 1. 删除配置文件 /etc/nginx 中的所有注释行和空行
+echo '1. 删除配置文件 /etc/nginx 中的所有注释行和空行'
+sed -i '/[:blank:]*#/d;/^$/d' nginx
+cat nginx
+
+# 2. 在配置文件 /etc/nginx 中所有不以 # 开头的行前面添加 * 符号
+echo ''
+echo '2. 在配置文件 /etc/nginx 中所有不以 # 开头的行前面添加 * 符号'
+sed -i 's/^[^#]/\*&/g' nginx
+cat nginx
+
+# 3. 删除 /etc/passwd 中的第 15 行
+echo ''
+echo '3. 删除 /etc/passwd 中的第 15 行'
+sed -i '15d' passwd
+cat passwd
+
+# 4. 删除 /etc/passwd 中第 5 行到以 ftp 开头的所有行的内容
+echo ''
+echo '4. 删除 /etc/passwd 中第 5 行到以 ftp 开头的所有行的内容'
+sed -i '5,/^ftp/d' passwd
+cat passwd
